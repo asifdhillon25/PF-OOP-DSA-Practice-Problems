c2c542bf9b3fcbf4841520aa7c4fcf3eb423b654
asifdhillon25 <asifdhillon25@gmail.com>
2025-01-31 16:45:00 +0500

    Add balance symbol stack project

---
+++ 3rd semester/BalanceSymbol/symbolBalance.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+#include "Stack.h"
+#include <string>
+#include "Functions.h"
+
+
+int main()
+{
+    Stack <char> MyStack;
+    string MyString;
+    char SymbolArr[] = { '(',')','{','}','[',']' };
+    
+    cout << "********Application - Balancing Symbols **********\n\n";
+    cout << "Enter eqution to check if its symbols balanced or not\n";
+    cin >> MyString;
+
+    if (isSymbolEqual(MyString, MyStack, SymbolArr))
+    {
+        cout << "\n\n\aEquation Has Balnced No of Symbols\n";
+    }
+    else
+    {
+        cout << "No of Symbols Are not Balanced\n";
+    }
+
+
+}
