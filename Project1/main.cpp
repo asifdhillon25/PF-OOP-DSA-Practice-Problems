7ad82526b4159dc66cf371633181d8f7e16ae994
asifdhillon25 <asifdhillon25@gmail.com>
2025-05-22 21:15:00 +0500

    Add Project1 configuration and resource files

---
+++ Project1/main.cpp
@@ -0,0 +1,47 @@
+#include<SDL.h>
+#include <SDL_image.h>
+#include "Class.h"
+#include "texturemanager.h"
+
+
+const int FPS = 60;
+const int DELAY_TIME = 1000.0f / FPS;
+
+int main(int argc, char* args[])
+{
+	Uint32 frameStart, frameTime;
+
+	std::cout << "game init attempt...\n";
+	if (TheGame::Instance()->init("Chapter 1", 0, 0,1280 , 720,
+		SDL_WINDOW_RESIZABLE))
+	{
+		std::cout << "game init success!\n";
+		while (TheGame::Instance()->running())
+		{
+			frameStart = SDL_GetTicks();
+			TheGame::Instance()->handleEvents();
+			TheGame::Instance()->update();
+			TheGame::Instance()->render();
+			frameTime = SDL_GetTicks() - frameStart;
+			if (frameTime < DELAY_TIME)
+			{
+				SDL_Delay((int)(DELAY_TIME - frameTime));
+			}
+			
+		}
+	}
+	else
+	{
+		std::cout << "game init failure - " << SDL_GetError() << "\n";
+		return -1;
+	}
+	
+	std::cout << "game closing...\n";
+	TheGame::Instance()->clean();
+	return 0;
+
+
+
+
+}
+
