9c455c5415fe9fb7ff9ae7df465132aaf46f72e5
asifdhillon25 <asifdhillon25@gmail.com>
2025-12-11 16:45:00 +0500

    Add bank with files project

---
+++ 2nd semester/Bank with Files/Bank.cpp
@@ -0,0 +1,5 @@
+//
+// Created by PC on 6/3/2023.
+//
+
+#include "Bank.h"
---
+++ 2nd semester/Bank with Files/Bank.h
@@ -0,0 +1,26 @@
+//
+// Created by PC on 6/3/2023.
+//
+
+#ifndef BANK_WITH_FILES_BANK_H
+#define BANK_WITH_FILES_BANK_H
+
+
+class Bank {
+
+    char Name[30];
+    int AccountNo;
+    double Balance=0;
+    int Password;
+public:
+    void writeAccount()
+    {
+
+
+    }
+
+
+};
+
+
+#endif //BANK_WITH_FILES_BANK_H
